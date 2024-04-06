608285a54fa0d796c61b91131d55acea9a3dee19
Aleena Thomas <aleenatom92@gmail.com>
2024-04-06 10:30:06 -0500

    added the user controller

--- bookmarksAPI/model/user.js
+++ bookmarksAPI/model/user.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-  user: String,
-  password: String,
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  bookmarks: [{type: mongoose.Types.ObjectId, ref: 'Bookmarks'}]
+}, {
+  timestamps: true, 
+  toJSON: {
+      transform: function(doc, ret){
+          delete ret.password
+          return ret
+}
+  }
 })
 
-const Users = mongoose.model('Users', userSchema)
-module.exports = Users;
+userSchema.pre('save', async function(next){
+  if(!this.isModified('password')) return next()
+  this.password = await bcrypt.hash(this.password, 6)
+  return next()
+})
+
+const User = mongoose.model('Users', userSchema)
+module.exports = User;
